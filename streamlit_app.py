d78ccb76e02726e077bada98e0393586145ebf3d
Ismail47727 <67604638+Ismail47727@users.noreply.github.com>
2024-12-06 01:34:57 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,15 @@
 # MODEL_DIR = 'https://raw.githubusercontent.com/Ismail47727/Fabric_Detection/main/best.pt'
 
 
-# def main():
-#     # Load the YOLO model
-#     model = YOLO(MODEL_DIR)
+def main():
+    # Load the YOLO model
+    model = YOLO(MODEL_DIR)
 
-#     st.sidebar.header("Fabric Defect Detection using YOLOv8\nMembers:\n\n21K-3118\n\n 21K-3010\n\n21K-3079\n\n21K-3115\n\n21K-3052\n\n DevOps Project")
-#     st.title("Real-time Fabric Defect Detection")
-#     st.write("""
-#     This app allows you to upload a fabric image or use your webcam for real-time fabric defect detection using the YOLOv8 model.
-#     """)
+    st.sidebar.header("Fabric Defect Detection using YOLOv8\nMembers:\n\n21K-3118\n\n 21K-3010\n\n21K-3079\n\n21K-3115\n\n21K-3052\n\n DevOps Project")
+    st.title("Real-time Fabric Defect Detection")
+    st.write("""
+    This app allows you to upload a fabric image or use your webcam for real-time fabric defect detection using the YOLOv8 model.
+    """)
 
 # # Sidebar Options
 # option = st.sidebar.radio("Choose Input Method", ("Browse Image", "Real-time Video"))
